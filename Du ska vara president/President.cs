20072b96d100a317065030c55827286fe8cd30c5
floxxui <60339661+floxxui@users.noreply.github.com>
2020-10-13 13:55:21 +0200

    commit2.0

--- Du ska vara president/President.cs
+++ Du ska vara president/President.cs
@@ -5,7 +5,11 @@ namespace Du_ska_vara_president
     public class President
     {
         public string name;
-
+        public int enviromentPriority;
+        public int defensePriority;
+        public int healthcarePriority;
+        public int corruption;
+        public int money;
 
     }
 }
--- Du ska vara president/Program.cs
+++ Du ska vara president/Program.cs
@@ -6,7 +6,30 @@ namespace Du_ska_vara_president
     {
         static void Main(string[] args)
         {
-            
+            President p = new President();
+
+            while (true)
+            {
+                System.Console.WriteLine("What should the president be called?");
+                
+                p.name = Console.ReadLine();
+
+                System.Console.WriteLine("Is the name " + p.name + " alright? (y/n)");
+
+                string playerAnswer = Console.ReadLine();
+
+                if (playerAnswer.ToLower() == "y")
+                {
+                    System.Console.WriteLine("The presidents name is now " + p.name);
+                    break;
+                }
+                else if (playerAnswer.ToLower() == "no")
+                {
+                    System.Console.WriteLine("");
+                }
+    
+                
+            }
         }
     }
 }
