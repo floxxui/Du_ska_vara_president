968c8b6768eed658a6e90f8c60ce86b848594f75
floxxui <60339661+floxxui@users.noreply.github.com>
2020-10-15 16:24:21 +0200

    commit3.0

--- Du ska vara president/President.cs
+++ Du ska vara president/President.cs
@@ -5,11 +5,27 @@ namespace Du_ska_vara_president
     public class President
     {
         public string name;
-        public int enviromentPriority;
-        public int defensePriority;
-        public int healthcarePriority;
+        public int[] priority = {0, 0, 0}; 
+        //Enviroment, Defense, Healthcare
         public int corruption;
         public int money;
+
+        public void GivingPriority(){
+            Random generator = new Random(100);
+
+            for (int i = 0; i < 3; i++)
+            {
+                priority[i] = generator.Next(100);
+
+            }
+        }
+
+//PassLegislation anropas för att försöka få presidenten att godkänna lagstiftning. Man stoppar in hur mycket 
+//lagstiftningen berör miljön (e), försvaret (d), sjukvård (h) som int-värden mellan 0–100. Man stoppar också in 
+//en summa för "bribe", som är hur mycket pengar man vill muta presidenten med.
+//För att passera lagstiftningen (returnera true) så måste värdena för e, d och h vara lägre än presidentens 
+//environmentPriority, defensePriority och healthcarePriority.
+
 
     }
 }
--- Du ska vara president/Program.cs
+++ Du ska vara president/Program.cs
@@ -8,28 +8,40 @@ namespace Du_ska_vara_president
         {
             President p = new President();
 
+            System.Console.WriteLine("What should the president be called?");
             while (true)
-            {
-                System.Console.WriteLine("What should the president be called?");
-                
+            {                
                 p.name = Console.ReadLine();
 
-                System.Console.WriteLine("Is the name " + p.name + " alright? (y/n)");
-
-                string playerAnswer = Console.ReadLine();
-
-                if (playerAnswer.ToLower() == "y")
+                while (true)
                 {
-                    System.Console.WriteLine("The presidents name is now " + p.name);
-                    break;
-                }
-                else if (playerAnswer.ToLower() == "no")
-                {
-                    System.Console.WriteLine("");
+                    System.Console.WriteLine("Is the name " + p.name + " alright? (y/n)");
+                    string playerAnswer = Console.ReadLine();
+
+                    if (playerAnswer.ToLower() == "y"){
+                       System.Console.WriteLine("The presidents name is now " + p.name);
+                       break;
+                    }
+                    else if (playerAnswer.ToLower() == "n"){
+                        System.Console.WriteLine("What should the president be called?");
+                        p.name = "";
+                        break;
+                    }
+                    else{
+                        System.Console.WriteLine("Please answer y or n");
+                    }
                 }
     
-                
+                if (p.name != ""){
+                    break;
+                }
             }
+            
+            p.GivingPriority();
+            System.Console.WriteLine("Enviroment priority: " + p.priority[0] + "\nDefense Priority" + p.priority[1] + "Healthcare Priority: " + p.priority[2]);
+
+            Console.ReadLine();
         }
+
     }
 }
